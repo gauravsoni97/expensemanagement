d3d170c521dbaa4658e1c8c3be8f39d64c323567
gauravsoni97 <gauravsoni8414@gmail.com>
2023-01-10 21:36:05 +0530

    Salary input in 502030 rule set

--- src/Components/TopBox/TopBox.jsx
+++ src/Components/TopBox/TopBox.jsx
@@ -1,22 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./topbox.css";
 
+let fiftyPercent = 50;
+let thirtyPercent = 30;
+let twentyPercent = 20;
+
 const TopBox = () => {
+  const [initialSalaryAmount, setInitialSalaryAmount] = useState(0);
+
+  const [fiftyPercentAmount, setFiftyPercentAmount] = useState(0);
+  const [thirtyPercentAmount, setthirtyPercentAmount] = useState(0);
+  const [twentyPercentAmount, settwentyPercentAmount] = useState(0);
+
+  const handleInitailSalaryAmount = (e) => {
+    setInitialSalaryAmount(e.target.value);
+  };
+
+  const handleTopForm = (e) => {
+    e.preventDefault();
+
+    setFiftyPercentAmount((fiftyPercent / 100) * initialSalaryAmount);
+    setthirtyPercentAmount((thirtyPercent / 100) * initialSalaryAmount);
+    settwentyPercentAmount((twentyPercent / 100) * initialSalaryAmount);
+  };
+
   return (
     <div className="top_box shadow rounded p-4 flex align-center justify-start">
       <div className="topbox_leftside">
-        <form>
+        <form onSubmit={handleTopForm}>
           <div className="input_fields">
             <label className="input_label">Select Month</label>
             <input type="month" />
           </div>
           <div className="input_fields">
             <label className="input_label">Enter Per Month Salary</label>
-            <input type="text" placeholder="100000" />
+            <input
+              type="number"
+              placeholder="100000"
+              value={initialSalaryAmount}
+              onChange={handleInitailSalaryAmount}
+            />
           </div>
-          <button type="submit" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Green</button>
-
+          <button
+            type="submit"
+            class=" w-full text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          >
+            Manage Money
+          </button>
         </form>
       </div>
       <div className="topbox_rightside ">
@@ -24,16 +55,28 @@ const TopBox = () => {
 
         <div className="parentOfNeedsWantsInvest">
           <div className="AmountRecievedBox">
-            <p className="needs_top_para"><b>50%</b> on Needs:</p>
-            <div className="needs_top_amount  shadow  bg-green-100 ">1552</div>
+            <p className="needs_top_para">
+              <b>50%</b> on Needs:
+            </p>
+            <div className="needs_top_amount  shadow  bg-green-100 ">
+              {fiftyPercentAmount}
+            </div>
           </div>
           <div className="AmountRecievedBox">
-            <p className="wants_top_para"><b>30%</b>  on Wants:</p>
-            <div className="needs_top_amount  shadow  bg-green-100 ">0</div>
+            <p className="wants_top_para">
+              <b>30%</b> on Wants:
+            </p>
+            <div className="needs_top_amount  shadow  bg-green-100 ">
+              {thirtyPercentAmount}
+            </div>
           </div>
           <div className="AmountRecievedBox">
-            <p className="invest_top_para"><b>20%</b>  on Invest:</p>
-            <div className="needs_top_amount  shadow  bg-green-100 ">252</div>
+            <p className="invest_top_para">
+              <b>20%</b> on Invest:
+            </p>
+            <div className="needs_top_amount  shadow  bg-green-100 ">
+              {twentyPercentAmount}
+            </div>
           </div>
         </div>
       </div>
