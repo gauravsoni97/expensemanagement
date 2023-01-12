f7d3cbd8a11db2783295051a83cadaba44bebccb
gauravsoni97 <gauravsoni8414@gmail.com>
2023-01-12 18:46:00 +0530

    input value for needs set

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Main from './Components/Main'
 
 const App = () => {
   return (
-    <div><Main/></div>
+    <div className='p-2'><Main/></div>
   )
 }
 
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,18 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Main.css";
+
+let fiftyPercent = 50;
+let thirtyPercent = 30;
+let twentyPercent = 20;
+
 const Main = () => {
+  // left side states ------------------
+  const [salaryInput, setSalaryInput] = useState();
+  const [needsAmountFromSalary, setNeedsAmountFromSalary] = useState(0);
+  const [wantsAmountFromSalary, setWantsAmountFromSalary] = useState(0);
+  const [investAmountFromSalary, setInvestAmountFromSalary] = useState(0);
+
+  // right side states --------------
+  const [addNeeds, setAddNeeds] = useState({
+    name:"",
+    price:""
+  });
+
+  // const [addWants, setAddWants] = useState();
+  // const [addInvest, setAddInvest] = useState();
+
+  // ----- handlers
+
+  const handleSalary = (e) => {
+    e.preventDefault();
+    setNeedsAmountFromSalary(Math.round((fiftyPercent / 100) * salaryInput));
+    setWantsAmountFromSalary(Math.round((thirtyPercent / 100) * salaryInput));
+    setInvestAmountFromSalary(Math.round((twentyPercent / 100) * salaryInput));
+    setSalaryInput("");
+  };
+
+  const handleBudgetForm = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="Main_Box flex align-start justify-start flex-wrap  bg-white rounded-xl">
       <div className="mainBox-leftside bg-[#FFE0CA] rounded-xl  p-3 ">
-        <h2 className="mainheading_topleft">Per Month Expenses</h2>
-        <form>
+        <h2 className="mainheading_topleft">Expense Management</h2>
+        <form onSubmit={handleSalary}>
           <div className="inputfield">
             <label>Select Month</label>
             <input type="month" placeholder="Jan-2023" />
           </div>
           <div className="inputfield">
             <label>Monthly Salary</label>
-            <input type="number" placeholder="100000" />
+            <input
+              type="number"
+              placeholder="100000"
+              value={salaryInput}
+              onChange={(e) => setSalaryInput(e.target.value)}
+            />
           </div>
 
           <button
@@ -27,17 +66,20 @@ const Main = () => {
           <i>50-30-20 Rule of Budgeting</i>
           <div className="split_in_needs_wants_invest">
             <div className="needs_from_salary bg-[#FFFBEC] my-3 rounded-xl p-3">
-              <p>50% on Needs: 5000</p>
+              <p>50% on Needs: {needsAmountFromSalary}</p>
             </div>
             <div className="wants_from_salary bg-[#FFFBEC] my-3 rounded-xl p-3">
-              <p>30% on Wants: 3000</p>
+              <p>30% on Wants: {wantsAmountFromSalary} </p>
             </div>
             <div className="invest_from_salary bg-[#FFFBEC] my-3 rounded-xl p-3">
-              <p>20% on Invest: 2000</p>
+              <p>20% on Invest: {investAmountFromSalary} </p>
             </div>
           </div>
         </div>
       </div>
+
+      {/* ----------------------------------------------------------- Right Side ----------------------------------------- */}
+
       <div className="mainBox-rightside p-3 ">
         <div className="filter_box  flex align-center justify-between my-4">
           <span>Filter By:</span>
@@ -45,14 +87,14 @@ const Main = () => {
         </div>
 
         <div className="input_amount_form_section">
-          <form>
+          <form onSubmit={handleBudgetForm}>
             <div className="inputfield inputfield_rightside">
               <label>Enter Name</label>
-              <input type="text" placeholder="Sugar" />
+              <input type="text" placeholder="Sugar" value={addNeeds.name} onChange={(e)=>setAddNeeds({...addNeeds, name:e.target.value})} />
             </div>
             <div className="inputfield inputfield_rightside">
               <label>Enter Amount</label>
-              <input type="number" placeholder="10000" />
+              <input type="number" placeholder="10000"  value={addNeeds.price} onChange={(e)=>setAddNeeds({...addNeeds, price:e.target.value})}  />
             </div>
             <button
               type="submit"
@@ -73,10 +115,9 @@ const Main = () => {
             </div>
             <div className="all_lists_parent">
               <div className="listed_item flex align-center justify-between my-2 py-1.5 px-2 rounded-lg bg-blue-50">
-                <p className="listed_item_name">Chini</p>
-                <p className="listed_item_price"> 234</p>
+                <p className="listed_item_name">{addNeeds.name}</p>
+                <p className="listed_item_price"> {addNeeds.price}</p>
                 <p className="listed_item_edit">
-                  
                   <i class="ri-edit-box-line"></i>
                 </p>
               </div>
